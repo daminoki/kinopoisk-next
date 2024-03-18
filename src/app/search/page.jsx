4a7e7bc8687a1105722362f3162de244f681f1d5
Daria Andreeva <d.andreeva@grechka.digital>
2024-03-18 18:00:42 +0400

    search in progress

---
+++ src/app/search/page.jsx
@@ -0,0 +1,6 @@
+import styles from './page.module.scss';
+export default function Search() {
+  return (
+    <div></div>
+  );
+}
--- src/components/layouts/Header/Search/SearchItem/index.jsx
+++ src/components/layouts/Header/Search/SearchItem/index.jsx
@@ -10,13 +10,14 @@ export const SearchItem = ({title, imgSrc, year, rating}) => {
       <Link href={'/'} className={styles['search-item__link']}>
         <div className={styles['search-item__img']}>
           {imgSrc && <Image src={imgSrc} alt={title} width={32} height={48} />}
+          {!imgSrc && <Image src={'/images/no-image.svg'} alt={title} width={32} height={48} />}
         </div>
         <div className={styles['search-item__info']}>
-          <>
-            <p className={styles['search-item__title']}>{title}</p>
+          <p className={styles['search-item__title']}>{title}</p>
+          <div className={styles['search-item__bottom']}>
+            <p className={styles['search-item__rating']}>{rating === 0 ? '-' : rating},</p>
             <p className={styles['search-item__release-year']}>{year}</p>
-          </>
-          <p className={styles['search-item__rating']}>{rating}</p>
+          </div>
         </div>
       </Link>
     </li>
