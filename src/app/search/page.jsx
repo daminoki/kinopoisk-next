b081191fe9400d50d063f87aa4c2b51ea59ba159
Daria Andreeva <d.andreeva@grechka.digital>
2024-03-19 17:19:20 +0400

    search in progress

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -4,7 +4,8 @@ import styles from './page.module.scss';
 import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { getSearchResult } from '@/api';
-
+import { SearchItem } from '@/components/pages/search/SearchItem';
+import Loader from '@/components/ui/Loader';
 
 export default function Search() {
   const queryParams = useSearchParams();
@@ -14,15 +15,19 @@ export default function Search() {
     limit: 10,
     query: ''
   });
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchSearchResult = async () => {
+    if (!searchParams.query) {
+      return;
+    }
+
     const result = await getSearchResult(searchParams);
 
     setSearchResult(result.docs);
   };
 
   useEffect(() => {
-    console.log(queryParams.get('query'));
     setSearchParams({
       ...searchParams,
       query: queryParams.get('query')
@@ -31,7 +36,9 @@ export default function Search() {
 
   useEffect(() => {
     async function fetchData() {
+      setIsLoading(true);
       await fetchSearchResult();
+      setIsLoading(false);
     }
 
     fetchData();
@@ -39,8 +46,28 @@ export default function Search() {
 
   return (
     <div className={styles.search}>
-      <div className={`${styles.search__container} container`}>
-        <h1>Результаты поиска</h1>
+      <div className='container'>
+        <h1 className={styles.search__title}>Результаты поиска</h1>
+
+        {isLoading && <Loader />}
+
+        {!isLoading && searchResult.length === 0 && (
+          <p className={styles.search__empty}>По вашему запросу ничего не найдено</p>
+        )}
+
+        {!isLoading && searchResult.length > 0 && (
+          <ul className={styles.search__result}>
+            {searchResult.map((item) => (
+              <SearchItem
+                key={item.id}
+                title={item.name}
+                imgSrc={item.poster.previewUrl}
+                year={item.year}
+                rating={item.rating.kp}
+              />
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
--- src/components/layouts/Header/Search/index.jsx
+++ src/components/layouts/Header/Search/index.jsx
@@ -4,19 +4,20 @@ import { debounce } from '@/utils/debounce';
 import { useState, useEffect, useMemo, useRef } from 'react';
 import { SearchItem } from '@/components/layouts/Header/Search/SearchItem';
 import { getSearchResult } from '@/api';
-import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import Loader from '@/components/ui/Loader';
 
 export const Search = () => {
   const [searchResult, setSearchResult] = useState([]);
   const [searchParams, setSearchParams] = useState({
     page: 1,
-    limit: 10,
+    limit: 5,
     query: ''
   });
   const [searchValue, setSearchValue] = useState('');
-  const ref = useRef();
   const [isDropdownOpened, setIsDropdownOpened] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const ref = useRef();
   const router = useRouter();
 
   const handleInputChange = (value) => {
@@ -47,7 +48,9 @@ export const Search = () => {
 
   useEffect(() => {
     async function fetchData() {
+      setIsLoading(true);
       await fetchSearchResult();
+      setIsLoading(false);
     }
 
     fetchData();
@@ -81,23 +84,37 @@ export const Search = () => {
         <SearchIcon/>
       </button>
 
-      {isDropdownOpened && (<div className={styles.search__result}>
-        <ul className={styles.search__list}>
-          {searchResult.map((result) => (
-            <SearchItem
-              key={result.id}
-              title={result.name}
-              imgSrc={result.poster.previewUrl}
-              year={result.year}
-              rating={result.rating.kp}
-            />
-          ))}
-        </ul>
+      <button type={'button'} className={styles['search__submit-mob']}>
+        <SearchIcon/>
+      </button>
 
-        {searchResult.length > 0 && (
-          <button className={styles['search__view-all']} onClick={(e) => handleSearch(e)}>
-              Показать все
-          </button>
+      {isDropdownOpened && (<div className={styles.search__result}>
+        {isLoading && <Loader/>}
+
+        {!isLoading && searchResult.length === 0 && (
+          <p className={styles['search__empty']}>
+            По вашему запросу ничего не найдено
+          </p>
+        )}
+
+        {!isLoading && searchResult.length > 0 && (
+          <>
+            <ul className={styles.search__list}>
+              {searchResult.map((result) => (
+                <SearchItem
+                  key={result.id}
+                  title={result.name}
+                  imgSrc={result.poster.previewUrl}
+                  year={result.year}
+                  rating={result.rating.kp}
+                />
+              ))}
+            </ul>
+
+            <button className={styles['search__view-all']} onClick={(e) => handleSearch(e)}>
+                  Показать все
+            </button>
+          </>
         )}
       </div>)
       }
